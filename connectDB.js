79184d8599116d11338c46bee8f72ae2fc6db9d8
TDN500 <6714140025@mut.ac.th>
2026-07-09 17:09:07 +0700

    dve3

---
+++ connectDB.js
@@ -0,0 +1,3 @@
+username:"Thatdanai";
+password:"6714140025";
+IP:"192.168.10.20";
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
     echo "Idk what am I doing man";
-    echo "Lmao";
-    echo "I'm so confused .exe";
-    echo "I'm catching up rn";
 ?>
